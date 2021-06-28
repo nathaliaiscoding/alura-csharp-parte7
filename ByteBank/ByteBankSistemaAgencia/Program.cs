4d55011c6c4123ce984ff011edbd5a878122865f
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-28 10:11:43 -0300

    aula 2

--- ByteBank/ByteBankSistemaAgencia/Program.cs
+++ ByteBank/ByteBankSistemaAgencia/Program.cs
@@ -7,9 +7,24 @@ namespace ByteBank.ByteBankSistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(123, 456789);
-            Console.WriteLine(conta.Conta);
-            Console.ReadLine();
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            lista.Adicionar(new ContaCorrente(345, 23462));
+            lista.Adicionar(new ContaCorrente(363, 22451));
+            lista.Adicionar(new ContaCorrente(735, 23552));
+            lista.Adicionar(new ContaCorrente(333, 23552));
+            lista.Adicionar(new ContaCorrente(456, 65423));
+            lista.Adicionar(new ContaCorrente(654, 87457));
+            lista.Adicionar(new ContaCorrente(334, 97348));
+            lista.Adicionar(new ContaCorrente(678, 65899));
+            lista.Adicionar(new ContaCorrente(735, 12223));
+            lista.Adicionar(new ContaCorrente(735, 98576));
+            lista.Adicionar(new ContaCorrente(735, 53534));
+            lista.Adicionar(new ContaCorrente(735, 56253));
+            lista.Adicionar(new ContaCorrente(735, 43657));
+            lista.Adicionar(new ContaCorrente(735, 77534));
+            lista.Adicionar(new ContaCorrente(735, 34436));
+            lista.Adicionar(new ContaCorrente(735, 98867));
         }
     }
 }
